3a0a1040ef003af7ea5abd84f5250b7a3d7593c6
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-03-29 16:56:42 -0400

    MVP completed

--- router/projects.js
+++ router/projects.js
@@ -18,13 +18,32 @@ router.get('/:id', (req, res) => {
 	const uniqueCohort = req.params.id;
 	db.from('projects')
 		.where({ id: uniqueCohort })
-		.then(cohort => {
+		.then(project => {
 			res.status(200).json(cohort);
 		})
 		.catch(err => {
 			res.status(500).json(err);
 		});
 });
+router.get("/:id/actions", (req, res) => {
+	const { id } = req.params;
+	db('projects').where('id', id)
+    .then(project => {
+      if(project) {
+        db('actions').where('project_id', id)
+          .then(actions => {
+            project[0].actions = actions;
+            res.status(200).json(project);
+          })
+          .catch(err => {
+            res.status(500).json({ error: ' error' })
+          })
+      } else {
+        res.status(404).json({ error: 'Failed to find project' })
+      }
+    })
+
+})
 
 router.post('/', (req, res) => {
 	const projects = req.body;
